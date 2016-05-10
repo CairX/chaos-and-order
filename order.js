18d8240f476f9db62882e2454cd9bc9c558a9486
thomas <thomas@pingpong.net>
2016-05-11 00:24:15 +0200

    clean out comments and rearrange a bit

--- order.js
+++ order.js
@@ -1,46 +1,41 @@
 (function () {
     "use strict";
-    var states = ["", "O", "X"];
 
-    var clicky = function (e) {
-        // console.log(e.target);
-        var index = states.indexOf(e.target.innerHTML) + 1;
-        // console.log(e.target.innerHTML);
-        // console.log(index);
-        // console.log(states.length);
+    var states = ["", "O", "X"];
+    var grid = [];
+    var gridElement = document.getElementsByClassName("grid")[0];
+
+    var cellEvent = function (event) {
+        var index = states.indexOf(event.target.innerHTML) + 1;
         if (index >= states.length) {
             index = 0;
         }
-        e.target.innerHTML = states[index];
+        event.target.innerHTML = states[index];
     };
 
-    var grid = [];
-    var e = document.getElementsByClassName("grid")[0];
+    var reset = function() {
+        var cells = document.getElementsByClassName("cell");
+        Array.prototype.forEach.call(cells, function(cell) {
+            cell.innerHTML = states[0];
+        });
+    };
+
     for (var y = 0; y < 5; y++) {
         var row = document.createElement("div");
         row.setAttribute("class", "row");
-
         grid[y] = [];
 
         for (var x = 0; x < 5; x++) {
             var cell = document.createElement("div");
             cell.setAttribute("class", "cell");
-            cell.addEventListener("click", clicky, false);
+            cell.addEventListener("click", cellEvent, false);
 
-            // Add to grid
             grid[y][x] = cell;
-            // Add to DOM
             row.appendChild(cell);
         }
 
-        e.appendChild(row);
+        gridElement.appendChild(row);
     }
 
-    document.getElementById("reset").addEventListener("click", function() {
-        for (var y = 0; y < grid.length; y++) {
-            for (var x = 0; x < grid[y].length; x++) {
-                grid[y][x].innerHTML = "";
-            }
-        }        
-    }, false);
+    document.getElementById("reset").addEventListener("click", reset, false);
 })();
